786344e39b0df7d957755f89fb4cdaba756b9366
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-09-24 19:11:13 +0530

    event added through button, but it ain't updating the water count

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,8 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,11 +11,21 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0;
-    Text(
-        text = "You have counted $count glasses",
-        modifier.padding(16.dp)
-    )
+    var count = 0;
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "You have counted $count glasses",
+        )
+
+        //adding events to update the state, event -> update state -> display state
+        Button(
+            onClick = { count++ },
+            Modifier.padding(top = 8.dp)
+        ) {
+            Text("Add One")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
