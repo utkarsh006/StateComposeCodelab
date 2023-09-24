ea94cd18bbade739e964e68ef408a1051a61078d
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-09-24 22:29:10 +0530

    state hoisting

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -7,44 +7,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+//Recomposition is re-running composables to update the composition when data changes.
+
+/* A value calculated by remember is stored in the Composition during the initial composition,
+and the stored value is kept across recompositions. */
+
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-
-    //Recomposition is re-running composables to update the composition when data changes.
-    /* A value calculated by remember is stored in the Composition during the initial composition,
-    and the stored value is kept across recompositions. */
-
+fun StatefulCounter(modifier: Modifier = Modifier) {
     val count: MutableState<Int> = rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(onIncrement = { count.value++ }, count = count.value)
+}
+//state going down, events going up, Unidirectional Data Flow (UDF)
+
+@Composable
+fun StatelessCounter(
+    modifier: Modifier = Modifier,
+    onIncrement: () -> Unit,
+    count: Int
+) {
     Column(modifier = modifier.padding(16.dp)) {
-        if (count.value > 0) {
+        if (count > 0) {
             /* If we press add button, counter increases to 1 and state changes.
                A recomposition is called, and screen gets recomposed with the new elements
              */
 
             Text(
-                text = "You have counted ${count.value} glasses",
+                text = "You have counted ${count} glasses",
             )
         }
 
         //adding events to update the state, event -> update state -> display state
         Button(
-            onClick = { count.value++ },
+            onClick = onIncrement,
             Modifier.padding(top = 8.dp)
         ) {
             Text("Add One")
         }
     }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun WaterCounterPreview() {
-    WaterCounter()
 }
