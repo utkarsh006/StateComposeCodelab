cac649eb8ff6930068727ee7e1c4bf678ce48685
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-10-14 10:41:11 +0530

    Close Button Functionality

--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -20,16 +20,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
-    var checkedState by rememberSaveable{ mutableStateOf(false) }
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
-        onCheckedChange = { newValue->
+        onCheckedChange = { newValue ->
             checkedState = newValue
         },
-        onClose = {}
+        onClose = onClose
     )
 }
 
@@ -37,7 +41,7 @@ fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
 fun WellnessTaskItem(
     taskName: String,
     checked: Boolean,
-    onCheckedChange:(Boolean) ->Unit,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -64,10 +68,10 @@ fun WellnessTaskItem(
 
 @Preview(showBackground = true)
 @Composable
-fun WellnessItemPreview(){
+fun WellnessItemPreview() {
     WellnessTaskItem(
         taskName = "Drink",
-        checked = true ,
+        checked = true,
         onCheckedChange = {},
         onClose = {}
     )
--- app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -3,24 +3,24 @@ package com.example.basicstatecodelab
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTasksList(
-    modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(modifier = modifier) {
-        items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+        items(items = list, key = { task -> task.id }) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                onClose = { onCloseTask(task) }
+            )
+
         }
 
     }
 }
 
-//call this list in Wellness Screen
-
-private fun getWellnessTasks() = List(30) { i ->
-    WellnessTask(i, "Task # $i")
-}
+// In this problem, we need to uniquely identify tasks based on their ID, not position
