e40f2c4b65c22b5ce6958d0c34e5e5ade9d2767f
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-10-14 10:18:13 +0530

    list of wellness tasks with checked behavior working

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,9 +1,14 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter(modifier)
+        WellnessTasksList()
+    }
+
 }
---
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,26 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn(modifier = modifier) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+
+    }
+}
+
+//call this list in Wellness Screen
+
+private fun getWellnessTasks() = List(30) { i ->
+    WellnessTask(i, "Task # $i")
+}
